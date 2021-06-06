bb65a2780b6d0ce6833224ec7f43e0e9732eedff
ChetanBolishetti <chetanbolishetti8@gmail.com>
2021-06-06 11:01:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/funshare/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.funshare
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun launch(view: View) {
+        val intent = Intent(this, MemeActivity::class.java)
+        startActivity(intent)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/funshare/MemeActivity.kt
@@ -0,0 +1,81 @@
+package com.example.funshare
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_meme.*
+import javax.sql.DataSource
+
+class MemeActivity : AppCompatActivity() {
+
+    var curImageUrl: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meme)
+    }
+
+    private fun loadMeme()
+    {
+        progressBar.visibility = View.VISIBLE
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                curImageUrl = response.getString("url")
+                Glide.with(this).load(curImageUrl).listener(object: RequestListener<Drawable> {
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: com.bumptech.glide.load.DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+
+                }).into(imageView)
+
+            },
+            Response.ErrorListener {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+            })
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+
+    fun shareMeme(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type="text/plane"
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey CheckOut This Cool Meme Shared By FunShare App Made By Chetan Bolishetti $curImageUrl")
+        val chooser =  Intent.createChooser(intent, "Share this meme using.....")
+        startActivity(chooser)
+    }
+
+}
